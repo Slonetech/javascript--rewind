5d5028823c6c7634da1954581330a5ec2679f997
Slonetech <kengeregordon@gmail.com>
2024-07-20 21:55:57 +0300

    worked with numbers

--- number.js
+++ number.js
@@ -1,7 +1,23 @@
 // change string to a number 
 
-let amount = '100'
+// let amount = '100'
 // amount =parseint(amount)
- amount =+amount 
+//  amount =+amount 
 // amount = Number (amount)
-console.log(amount)
+
+// change no to string
+// let solution = 99.5
+// solution = solution.toString()
+// solution = `${solution}`
+// solution = String(solution)
+
+// change string to decimal
+// solution = parseFloat(solution)
+// console.log(solution, typeof solution)
+
+// convert to boolean
+// 0 is a falsy value
+// 1 is a truthy value
+let amount = 0
+amount = Boolean(amount)
+console.log(amount, typeof amount)
