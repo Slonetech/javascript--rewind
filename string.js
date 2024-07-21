f4342d27455eac22b1fd8ed1030442c587693bd6
Slonetech <kengeregordon@gmail.com>
2024-07-21 09:22:39 +0300

    working on strings

---
+++ string.js
@@ -0,0 +1,16 @@
+// const s = new String('Hello World');
+
+//  x = s.length    //--this is a property
+
+// x = s[1];
+
+// x = s.charAt(1);  // this is a method has parenthesis
+
+// x = s.__proto__ ;
+
+
+// change the first letter to uppercase
+ const myString = 'developer'
+x = myString.charAt(0).toLocaleUpperCase()+myString.slice(1)
+
+console.log(x)
